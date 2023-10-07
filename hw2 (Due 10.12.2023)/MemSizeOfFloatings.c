1b432a64b086bdaa516198eedf6ae79e3aed66f3
SamWang8891 <73735177+SamWang8891TW@users.noreply.github.com>
2023-10-07 11:36:23 +0800

    Create MemSizeOfFloatings.c

---
+++ hw2 (Due 10.12.2023)/MemSizeOfFloatings.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+
+int main() {
+    printf("float: memory space = %zu bytes.\n", sizeof(float));
+    printf("double: memory space = %zu bytes.\n", sizeof(double));
+    printf("long double: memory space = %zu bytes.\n", sizeof(long double));
+    
+    
+    return 0;
+}
