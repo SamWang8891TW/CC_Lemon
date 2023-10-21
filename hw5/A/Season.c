7f25d5deb832c028859a4397a83e4cab7eadb43f
SamWang8891TW <g348.8891@gmail.com>
2023-10-21 03:09:18 +0000

    Updates!

--- hw5/A/Season.c
+++ hw5/A/Season.c
@@ -1,1 +1,29 @@
-//努力中...
+#include <stdio.h>
+
+
+int main(){
+    int input;
+
+    printf("Please input a month: ");
+    scanf("%d", &input);
+
+    switch(input){
+        case 3 ... 5:
+            printf("It's Spring\n");
+            break;
+        case 6 ... 8:
+            printf("It's Summer\n");
+            break;
+        case 9 ... 11:
+            printf("It's Autumn\n");
+            break;
+        case 12: //因為沒有break; 所以會繼續執行下面的東西
+        case 1 ... 2:
+            printf("It's Winter\n");
+            break;
+    }
+
+
+
+    return 0;
+}
