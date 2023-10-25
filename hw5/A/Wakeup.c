88757c3f9e4e53097154d1740f1c733858628501
SamWang8891TW <g348.8891@gmail.com>
2023-10-25 11:22:42 +0000

    minor updates

--- hw5/A/Wakeup.c
+++ hw5/A/Wakeup.c
@@ -7,27 +7,27 @@ int main() {
   scanf("%d", &input);
 
   switch (input) {
-  case 0:
-    printf("You have to wake up at 10:00.\n");
-    break;
-  case 1:
-    printf("You have to wake up at 7:30.\n");
-    break;
-  case 2:
-    printf("You have to wake up at 8:30.\n");
-    break;
-  case 3:
-    printf("You have to wake up at 8:30.\n");
-    break;
-  case 4:
-    printf("You have to wake up at 7:30.\n");
-    break;
-  case 5:
-    printf("You have to wake up at 8:30.\n");
-    break;
-  case 6:
-    printf("You have to wake up at 10:00.\n");
-    break;
+    case 0:
+      printf("You have to wake up at 10:00.\n");
+      break;
+    case 1:
+      printf("You have to wake up at 7:30.\n");
+      break;
+    case 2:
+      printf("You have to wake up at 8:30.\n");
+      break;
+    case 3:
+      printf("You have to wake up at 8:30.\n");
+      break;
+    case 4:
+      printf("You have to wake up at 7:30.\n");
+      break;
+    case 5:
+      printf("You have to wake up at 8:30.\n");
+      break;
+    case 6:
+      printf("You have to wake up at 10:00.\n");
+      break;
   }
 
   return 0;
